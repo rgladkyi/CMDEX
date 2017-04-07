51967f4e7fa25d76547e86c201f7b72beeda69e6
rgladkyi <roman.gladkuy@gmail.com>
2017-04-07 17:13:22 +0300

    Interfaces

--- Interfaces/git.bat
+++ Interfaces/git.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if not exist "C:\CMDEX\Tools\PortableGit\cmd\git.exe" (call C:\CMDEX\WinOpenInstaller\InstallScripts\git_install.bat "C:\CMDEX\Tools\")
+if not exist "C:\CMDEX\Tools\PortableGit\cmd\git.exe" (call C:\CMDEX\WinOpenInstaller\InstallScripts\git_install.bat "C:\CMDEX\Tools")
 if exist "C:\CMDEX\Tools\PortableGit\cmd\git.exe" (call "C:\CMDEX\Tools\PortableGit\cmd\git.exe" %*) else (echo Installation failed.. Check itself.)
 
 
