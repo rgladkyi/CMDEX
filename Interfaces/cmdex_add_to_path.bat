3ff6556a7aead88e049ae60f0a3d21ad9dfcdf01
rgladkyi <roman.gladkuy@gmail.com>
2017-04-14 14:02:39 +0300

    Added/Updated cmdex_add_to_path.bat cmdex_remove_from_path.bat.

--- Interfaces/cmdex_add_to_path.bat
+++ Interfaces/cmdex_add_to_path.bat
@@ -2,6 +2,18 @@
 
 call cmdex_remove_from_path
 
-setx /m PATH "%PATH%;C:\CMDEX\Interfaces"
+rem setx PATH "%PATH%;C:\CMDEX\Interfaces" /m 
+
+rem REG ADD HKCU\SOFTWARE\Ditto /v DittoHotKey /f /t REG_DWORD /d 704
+rem REG QUERY HKCU\SOFTWARE\Ditto /v DittoHotKey 
+ 
+rem reg add ... HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment\Path
+
+
+
+set UPDATED_PATH="%PATH%;C:\CMDEX\Interfaces"
+
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /f /t REG_SZ /d %UPDATED_PATH%
+
 
 
--- Interfaces/cmdex_remove_from_path.bat
+++ Interfaces/cmdex_remove_from_path.bat
@@ -1,13 +1,20 @@
 @echo off
 
-set LOCAL_PATH=%PATH%
-echo %LOCAL_PATH%
+rem set LOCAL_PATH=%PATH%
+rem echo %LOCAL_PATH%
 
-set UPDATED_LOC_PATH=%LOCAL_PATH:;C:\CMDEX\Interfaces;=%
-set UPDATED_LOC_PATH_1=%UPDATED_LOC_PATH:;C:\CMDEX\Interfaces=%
-echo %UPDATED_LOC_PATH_1%
+rem set UPDATED_LOC_PATH=%LOCAL_PATH:;C:\CMDEX\Interfaces;=%
+rem set UPDATED_LOC_PATH_1=%UPDATED_LOC_PATH:;C:\CMDEX\Interfaces=%
+rem echo %UPDATED_LOC_PATH_1%
 
-set PATH=%UPDATED_LOC_PATH%
-setx /m PATH %UPDATED_LOC_PATH%
+rem set PATH=%UPDATED_LOC_PATH%
+rem setx /m PATH %UPDATED_LOC_PATH%
+
+rem REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /f /t REG_SZ /d %UPDATED_LOC_PATH_1%
+
+
+set UPDATED_PATH="%PATH:;C:\CMDEX\Interfaces=%"
+
+REG ADD "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /f /t REG_SZ /d %UPDATED_PATH%
 
 
