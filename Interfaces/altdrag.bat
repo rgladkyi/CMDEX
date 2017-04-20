00cd4506300bdf14f6efa5082edfb62c21f48077
rgladkyi <roman.gladkuy@gmail.com>
2017-04-20 19:37:49 +0300

    Added/Updated altdrag.bat.

--- Interfaces/altdrag.bat
+++ Interfaces/altdrag.bat
@@ -1,5 +1,17 @@
 @echo off
 
-start "" "C:\CMDEX\Tools\AltDrag\AltDrag.exe" %*
+if not exist "C:\CMDEX\Tools\AltDrag\AltDrag.exe" (call C:\CMDEX\WinOpenInstaller\InstallScripts\altdrag_install.bat "C:\CMDEX\Tools")
+
+if exist "C:\CMDEX\Tools\AltDrag\AltDrag.exe" (start "" "C:\CMDEX\Tools\AltDrag\AltDrag.exe" %*) else (echo Installation failed.. Check itself. && goto Exit)
+goto Instructions
+
+:Instructions
+echo.
+echo Press 'Alt + <Left Mouse> ' to drag windows.
+goto End
+
+:Exit
+
+:End
 
 
