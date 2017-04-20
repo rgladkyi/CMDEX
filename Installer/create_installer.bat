4d515955852e627a635cac224f857617c8f6af30
rgladkyi <roman.gladkuy@gmail.com>
2017-04-21 00:34:58 +0300

    Added/Updated create_installer.bat.

--- Installer/create_installer.bat
+++ Installer/create_installer.bat
@@ -13,11 +13,12 @@ call C:\CMDEX\Release\create_zip_from_exe.bat
 
 call C:\CMDEX\Data\clear_data.bat
 call C:\CMDEX\Release\publish_release.bat
+call C:\CMDEX\Release\post_release.bat
 
-call echo.
-call Need wait couple of minutess while new release will be updated on the server.
-call And then press any key.
-call echo.
-call C:\CMDEX\Release\check_update.bat
+echo.
+rem echo Need wait couple of minutess while new release will be updated on the server.
+rem echo And then press any key.
+rem echo echo.
+rem callC:\CMDEX\Release\check_update.bat
 
 
