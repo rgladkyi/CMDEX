c2e0e9c35180c0ab064d97f03a7b88f6e7e93bfa
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 01:34:36 +0300

    Added/Updated publish_release.bat.

--- Release/publish_release.bat
+++ Release/publish_release.bat
@@ -1,28 +1,28 @@
 @echo off
 
 echo Copy to disk drives G, E and F:
-copy /y "C:\CMDEX\Release\VersionId.txt" G:\
-copy /y "C:\CMDEX\Release\CMDEXsetup.exe" G:\
-copy /y "C:\CMDEX\Release\CMDEXfolder.zip" G:\
+copy /y "%CMDEX_ROOT_DIR%\Release\VersionId.txt" G:\
+copy /y "%CMDEX_ROOT_DIR%\Release\CMDEXsetup.exe" G:\
+copy /y "%CMDEX_ROOT_DIR%\Release\CMDEXfolder.zip" G:\
 
-copy /y "C:\CMDEX\Release\VersionId.txt" E:\
-copy /y "C:\CMDEX\Release\CMDEXsetup.exe" E:\
-copy /y "C:\CMDEX\Release\CMDEXfolder.zip" E:\
+copy /y "%CMDEX_ROOT_DIR%\Release\VersionId.txt" E:\
+copy /y "%CMDEX_ROOT_DIR%\Release\CMDEXsetup.exe" E:\
+copy /y "%CMDEX_ROOT_DIR%\Release\CMDEXfolder.zip" E:\
 
-copy /y "C:\CMDEX\Release\VersionId.txt" F:\
-copy /y "C:\CMDEX\Release\CMDEXsetup.exe" F:\
-copy /y "C:\CMDEX\Release\CMDEXfolder.zip" F:\
+copy /y "%CMDEX_ROOT_DIR%\Release\VersionId.txt" F:\
+copy /y "%CMDEX_ROOT_DIR%\Release\CMDEXsetup.exe" F:\
+copy /y "%CMDEX_ROOT_DIR%\Release\CMDEXfolder.zip" F:\
 
-set /p VersionId_txt=<"C:\CMDEX\Release\VersionId.txt"
+set /p VersionId_txt=<"%CMDEX_ROOT_DIR%\Release\VersionId.txt"
 
 call pause3
 
 echo Press Ctrl+C if there is no need to commit this release.
 call pause3
 
-call git add "C:\CMDEX\Release\VersionId.txt"
-call git add "C:\CMDEX\Release\CMDEXsetup.exe"
-call git add "C:\CMDEX\Release\CMDEXfolder.zip" 
+call git add "%CMDEX_ROOT_DIR%\Release\VersionId.txt"
+call git add "%CMDEX_ROOT_DIR%\Release\CMDEXsetup.exe"
+call git add "%CMDEX_ROOT_DIR%\Release\CMDEXfolder.zip" 
 call git status
 call git commit -m "Updated Installer to version %VersionId_txt%."
 
