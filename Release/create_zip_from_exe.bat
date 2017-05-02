c723aed898c4376a6d755fab24132ca1dc04a608
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 01:32:57 +0300

    Added/Updated create_zip_from_exe.bat.

--- Release/create_zip_from_exe.bat
+++ Release/create_zip_from_exe.bat
@@ -2,24 +2,24 @@
 set CD_TO_COME_BACK_create_zip_from_exe=%cd%
 echo %CD_TO_COME_BACK_create_zip_from_exe%
 
-call cd "C:\CMDEX\Release\"
+call cd "%CMDEX_ROOT_DIR%\Release\"
 
-rmdir /s /q "C:\CMDEX\Release\CMDEXfolder.zip"
+rmdir /s /q "%CMDEX_ROOT_DIR%\Release\CMDEXfolder.zip"
 
 call exe_unzip.bat CMDEXsetup.exe -oCMDEX 
 
-rmdir /s /q "C:\CMDEX\Release\CMDEX\$PLUGINSDIR"
+rmdir /s /q "%CMDEX_ROOT_DIR%\Release\CMDEX\$PLUGINSDIR"
 
 echo Add nessesary files from CMDEX root folder..
-copy /y "C:\CMDEX\cmdex_install.bat" "C:\CMDEX\Release\CMDEX\"
-copy /y "C:\CMDEX\doit.bat" "C:\CMDEX\Release\CMDEX\"
-copy /y "C:\CMDEX\install.bat" "C:\CMDEX\Release\CMDEX\"
-copy /y "C:\CMDEX\README.md" "C:\CMDEX\Release\CMDEX\"
-copy /y "C:\CMDEX\License.txt" "C:\CMDEX\Release\CMDEX\"
+copy /y "%CMDEX_ROOT_DIR%\cmdex_install.bat" "\Release\CMDEX\"
+copy /y "%CMDEX_ROOT_DIR%\doit.bat" "\Release\CMDEX\"
+copy /y "%CMDEX_ROOT_DIR%\install.bat" "\Release\CMDEX\"
+copy /y "%CMDEX_ROOT_DIR%\README.md" "\Release\CMDEX\"
+copy /y "%CMDEX_ROOT_DIR%\License.txt" "\Release\CMDEX\"
 
 call zip CMDEX
 
-rmdir /s /q "C:\CMDEX\Release\CMDEX"
+rmdir /s /q "%CMDEX_ROOT_DIR%\Release\CMDEX"
 
 call mv CMDEX.zip CMDEXfolder.zip
 
