d33f7ea9845ec5c51b7769f43109e19432ea93bf
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 01:28:01 +0300

    Added/Updated cmdex_get_set_root_dir.bat.

---
+++ Interfaces/cmdex_get_set_root_dir.bat
@@ -0,0 +1,19 @@
+@echo off
+
+if [%CMDEX_ROOT_DIR%]==[] (goto GetCMDEXRootDir) else (goto ReturnCMDEXRootDir)
+
+:GetCMDEXRootDir
+FOR /F "usebackq tokens=2,* skip=2" %%L IN (
+    `REG QUERY "HKCU\Software\Conjeside\CMDEX" /v RootDirectory`
+) DO SET REG_CMDEX_ROOT_DIR=%%M
+
+echo %REG_CMDEX_ROOT_DIR%
+if not exist %REG_CMDEX_ROOT_DIR% (goto End) else (set CMDEX_ROOT_DIR=%REG_CMDEX_ROOT_DIR%)
+goto ReturnCMDEXRootDir
+
+:ReturnCMDEXRootDir
+rem set CMDEX_ROOT_DIR=%REG_CMDEX_ROOT_DIR%
+
+:End
+
+
