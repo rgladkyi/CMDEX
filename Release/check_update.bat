f51011ebe7c23cb9a8889be69b2786b2ae6a5ab5
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 01:33:08 +0300

    Added/Updated check_update.bat.

--- Release/check_update.bat
+++ Release/check_update.bat
@@ -1,7 +1,7 @@
 rem @echo off
 
-call C:\CMDEX\Release\seem_that_altered_version.bat
-call C:\CMDEX\Data\clear_data.bat
+call %CMDEX_ROOT_DIR%\Release\seem_that_altered_version.bat
+call %CMDEX_ROOT_DIR%\Data\clear_data.bat
 
 call update
 
