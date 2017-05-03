490d1110db22dd34362ba763bfbfc64b847a4cf3
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 15:56:56 +0300

    Added/Updated cmdex_add_settings_to_registry.bat.

--- Interfaces/cmdex_add_settings_to_registry.bat
+++ Interfaces/cmdex_add_settings_to_registry.bat
@@ -1,10 +1,10 @@
 rem @echo off
 
-set CMDEX_ROOT_DIR=%1
+set CMDEX_ROOT_DIR_REGISTRY=%1
 echo %date% %time% >> C:\cmdex_debug.txt
 echo %1 >> C:\cmdex_debug.txt
-echo %CMDEX_ROOT_DIR% >> C:\cmdex_debug.txt
+echo %CMDEX_ROOT_DIR_REGISTRY% >> C:\cmdex_debug.txt
 pause
 
-REG ADD "HKCU\Software\Conjeside\CMDEX" /v RootDirectory /f /t REG_SZ /d %CMDEX_ROOT_DIR%
+REG ADD "HKCU\Software\Conjeside\CMDEX" /v RootDirectory /f /t REG_SZ /d %CMDEX_ROOT_DIR_REGISTRY%
 pause
