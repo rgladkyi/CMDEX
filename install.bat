9e65410d30897b7b44f8e4469f3cd1f623746d28
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 03:01:39 +0300

    Added/Updated install.bat.

--- install.bat
+++ install.bat
@@ -1,5 +1,9 @@
 @echo off
 
+set INSTALL_DIR=%1
+if [%INSTALL_DIR%]==[] (echo Need to specify install directory as first parameter. && goto Error) else (goto DoInstall)
+
+:DoInstall
 mkdir C:\CMDEX\Aliases
 mkdir C:\CMDEX\Automation
 mkdir C:\CMDEX\Data
@@ -14,6 +18,9 @@ mkdir C:\CMDEX\Tools
 mkdir C:\CMDEX\Trash
 mkdir C:\CMDEX\Wall
 
+call cmdex_add_settings_to_registry.bat %INSTALL_DIR%
+call cmdex_get_set_root_dir.bat
+
 call "C:\CMDEX\Installer\TerminateRunningProcessesBeforeItsUpdate.bat"
 call "C:\CMDEX\Interfaces\sudo.bat" "C:\CMDEX\Interfaces\cmdex_add_to_path.bat"
 call "C:\CMDEX\Interfaces\cmdex_create_shortcuts.bat"
@@ -22,3 +29,10 @@ echo.
 echo Completed.
 
 pause
+goto End
+
+:End
+
+:Error
+
+
