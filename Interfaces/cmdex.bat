925b38f579a75e66bc0ab131a9c7d0a2624dbdad
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 14:13:56 +0300

    Added/Updated cmdex.bat.

--- Interfaces/cmdex.bat
+++ Interfaces/cmdex.bat
@@ -49,7 +49,7 @@ echo %date% > %PATH_FILE_DATE_LAST_OPENED%
 goto StartCmd
 
 :StartCmd
-cd  %2 %3 %4 %5 %6 %7 %8 %9
+call c  %2 %3 %4 %5 %6 %7 %8 %9
 rem cmd
 cmd /k cmdex_get_set_root_dir.bat
 goto End
