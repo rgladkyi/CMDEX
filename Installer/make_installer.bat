3f1be7d3c94a41680b64681335dd91ce098c527c
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 16:34:14 +0300

    Added/Updated make_installer.bat.

--- Installer/make_installer.bat
+++ Installer/make_installer.bat
@@ -9,7 +9,7 @@ echo New version is %VersionId_txt%.
 set DIRECTORY_PATH_TO_RETURN=%cd%
 echo %DIRECTORY_PATH_TO_RETURN%
 
-cd "C:\Program Files (x86)\NSIS\Bin\"
-call cmdex_make_installer
+cd "%CMDEX_ROOT_DIR%\Installer"
+call cmdex_make_installer.bat
 
 cd %DIRECTORY_PATH_TO_RETURN%
