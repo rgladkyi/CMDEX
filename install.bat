e1a600f7f1cccd68eafaf2b552840e8bb01cbc31
rgladkyi <roman.gladkuy@gmail.com>
2017-05-03 16:05:41 +0300

    Added/Updated install.bat.

--- install.bat
+++ install.bat
@@ -1,29 +1,33 @@
 @echo off
 
 set INSTALL_DIR=%1
-if [%INSTALL_DIR%]==[] (echo Need to specify install directory as first parameter. && goto Error) else (goto DoInstall)
+if [%INSTALL_DIR%]==[] (echo Need to specify install directory as first parameter. && goto Error) else (goto SetInstallDir)
 
-:DoInstall
-mkdir C:\CMDEX\Aliases
-mkdir C:\CMDEX\Automation
-mkdir C:\CMDEX\Data
-mkdir C:\CMDEX\Documentation
-mkdir C:\CMDEX\History
-mkdir C:\CMDEX\Installer
-mkdir C:\CMDEX\Interfaces
-mkdir C:\CMDEX\Release
-mkdir C:\CMDEX\Resources
-mkdir C:\CMDEX\TestCases
-mkdir C:\CMDEX\Tools
-mkdir C:\CMDEX\Trash
-mkdir C:\CMDEX\Wall
-
+:SetInstallDir
 call cmdex_add_settings_to_registry.bat %INSTALL_DIR%
 call cmdex_get_set_root_dir.bat
+goto DoInstall
 
-call "C:\CMDEX\Installer\TerminateRunningProcessesBeforeItsUpdate.bat"
-call "C:\CMDEX\Interfaces\sudo.bat" "C:\CMDEX\Interfaces\cmdex_add_to_path.bat"
-call "C:\CMDEX\Interfaces\cmdex_create_shortcuts.bat"
+:DoInstall
+mkdir "%CMDEX_ROOT_DIR%\Aliases"
+mkdir "%CMDEX_ROOT_DIR%\Automation"
+mkdir "%CMDEX_ROOT_DIR%\Data"
+mkdir "%CMDEX_ROOT_DIR%\Documentation"
+mkdir "%CMDEX_ROOT_DIR%\History"
+mkdir "%CMDEX_ROOT_DIR%\Installer"
+mkdir "%CMDEX_ROOT_DIR%\Interfaces"
+mkdir "%CMDEX_ROOT_DIR%\Release"
+mkdir "%CMDEX_ROOT_DIR%\Resources"
+mkdir "%CMDEX_ROOT_DIR%\TestCases"
+mkdir "%CMDEX_ROOT_DIR%\Tools"
+mkdir "%CMDEX_ROOT_DIR%\Trash"
+mkdir "%CMDEX_ROOT_DIR%\Wall"
+
+
+
+call "%CMDEX_ROOT_DIR%\Installer\TerminateRunningProcessesBeforeItsUpdate.bat"
+call "%CMDEX_ROOT_DIR%\Interfaces\sudo.bat" "%CMDEX_ROOT_DIR%\Interfaces\cmdex_add_to_path.bat"
+call "%CMDEX_ROOT_DIR%\Interfaces\cmdex_create_shortcuts.bat"
 
 echo.
 echo Completed.
