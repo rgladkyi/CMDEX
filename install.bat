7e4d396b8f72b5b1cae7c4d8588ef573cc078b15
rgladkyi <roman.gladkuy@gmail.com>
2017-05-07 05:15:43 +0300

    Added/Updated install.bat.

--- install.bat
+++ install.bat
@@ -1,11 +1,16 @@
 @echo off
 
 set INSTALL_DIR=%1
-if [%INSTALL_DIR%]==[] (echo Need to specify install directory as first parameter. && goto Error) else (goto SetInstallDir)
+echo %INSTALL_DIR%
+
+rem if [%INSTALL_DIR%]==[] (echo Need to specify install directory as first parameter. && goto Error) else (goto SetInstallDir)
+
+set CMDEX_ROOT_DIR=%INSTALL_DIR:"=%
+echo %CMDEX_ROOT_DIR%
 
 :SetInstallDir
-call cmdex_add_settings_to_registry.bat %INSTALL_DIR%
-call cmdex_get_set_root_dir.bat
+call "%CMDEX_ROOT_DIR%\Interfaces\cmdex_add_settings_to_registry.bat" %INSTALL_DIR%
+call "%CMDEX_ROOT_DIR%\Interfaces\cmdex_get_set_root_dir.bat"
 goto DoInstall
 
 :DoInstall
@@ -26,7 +31,10 @@ mkdir "%CMDEX_ROOT_DIR%\Wall"
 
 
 call "%CMDEX_ROOT_DIR%\Installer\TerminateRunningProcessesBeforeItsUpdate.bat"
-call "%CMDEX_ROOT_DIR%\Interfaces\sudo.bat" "%CMDEX_ROOT_DIR%\Interfaces\cmdex_add_to_path.bat"
+rem call "%CMDEX_ROOT_DIR%\Interfaces\rt.bat" "%CMDEX_ROOT_DIR%\Interfaces\cmdex_add_to_path.bat" %INSTALL_DIR%
+call "%CMDEX_ROOT_DIR%\Interfaces\cmdex_add_to_path.bat" %INSTALL_DIR%
+
+
 call "%CMDEX_ROOT_DIR%\Interfaces\cmdex_create_shortcuts.bat"
 
 echo.
