35b626906436f36d17089505aeaa952dc9258e0d
rgladkyi <roman.gladkuy@gmail.com>
2017-05-07 05:16:06 +0300

    Added/Updated rt.bat.

--- Interfaces/rt.bat
+++ Interfaces/rt.bat
@@ -3,6 +3,6 @@
 echo To execute in: "%cd%"
 echo Planned to execute: %*
 
-call sudo.lnk /c run_externally "%cd%" %*
+call "%CMDEX_ROOT_DIR%\Interfaces\sudo.lnk" /c "%CMDEX_ROOT_DIR%\Interfaces\run_externally.bat" "%cd%" %*
 
 
