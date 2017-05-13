ce4990efe8112807a0209ac755dae2adc8af33a1
rgladkyi <roman.gladkuy@gmail.com>
2017-05-13 12:05:35 +0300

    Added/Updated updatex.bat.

--- Interfaces/updatex.bat
+++ Interfaces/updatex.bat
@@ -1,6 +1,6 @@
-@echo off
+rem @echo off
 
 call cp "%CMDEX_ROOT_DIR%\Interfaces\cmdex_update.bat" "%TEMP%\cmdex_update_temp.bat"
-(call sudo "%TEMP%\cmdex_update_temp.bat") && goto :EOF
+(call sudo.lnk /c "%TEMP%\cmdex_update_temp.bat") && goto :EOF
 
 
