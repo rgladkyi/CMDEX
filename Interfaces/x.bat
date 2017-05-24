0a0a71fa77f75b4c0bbb50a378b7a58eabf963b3
rgladkyi <roman.gladkuy@gmail.com>
2017-05-25 02:07:13 +0300

    Added/Updated x.bat.

--- Interfaces/x.bat
+++ Interfaces/x.bat
@@ -1,5 +1,5 @@
 @echo off && call cmdex_pre_run.bat
 
-if [%1]==[] (start "" ADMIN.lnk %cd%) else (start "" ADMIN.lnk %*)
+if [%1]==[] (start "" ADMIN.lnk /k cd "%cd%") else (start "" ADMIN.lnk %*)
 
 
