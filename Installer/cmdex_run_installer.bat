58d32e9a48284f2885d2e9a86e53d255f458439a
rgladkyi <roman.gladkuy@gmail.com>
2017-05-24 23:55:23 +0300

    Added/Updated Installer/cmdex_run_installer.bat.

--- Installer/cmdex_run_installer.bat
+++ Installer/cmdex_run_installer.bat
@@ -1,6 +1,6 @@
 
-rem "%CMDEX_ROOT_DIR%\Release\CMDEXsetup.exe"
-"%CMDEX_ROOT_DIR%\Installer\NSIS\Scripts\CMDEXsetup.exe"
+"%CMDEX_ROOT_DIR%\Release\CMDEXsetup.exe"
+rem "%CMDEX_ROOT_DIR%\Installer\NSIS\Scripts\CMDEXsetup.exe"
 
 
 
