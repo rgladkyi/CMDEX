42869e87572bbf5caf8ab27d2e502d1ccb8d4766
rgladkyi <roman.gladkuy@gmail.com>
2017-05-24 22:50:04 +0300

    Added/Updated prepare_release.bat.

--- prepare_release.bat
+++ prepare_release.bat
@@ -3,4 +3,4 @@
 rem call cmdex_get_set_root_dir.bat
 rem call "%CMDEX_ROOT_DIR%\Installer\create_installer.bat"
 
-call "C:\CMDEX Preset\Installer\create_installer.bat"
+call "%CMDEX_ROOT_DIR%\Installer\create_installer.bat"
