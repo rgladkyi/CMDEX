81f582f79804e6deeafc90b08f880d5cb1204056
rgladkyi <roman.gladkuy@gmail.com>
2017-05-25 20:18:39 +0300

    Added/Updated gvim.bat.

--- Interfaces/gvim.bat
+++ Interfaces/gvim.bat
@@ -1,7 +1,7 @@
 @echo off && call cmdex_pre_run.bat
 
 :Check
-call check_tool "%CMDEX_ROOT_DIR%\Tools\Vim\vim80\gvim.exe" "%CMDEX_ROOT_DIR%\WinOpenInstaller\InstallScripts\vim_install.bat"  "%CMDEX_ROOT_DIR%\Tools\"
+call check_tool "%CMDEX_ROOT_DIR%\Tools\Vim\vim80\gvim.exe" "%CMDEX_ROOT_DIR%\WinOpenInstaller\InstallScripts\vim_install.bat"  "%CMDEX_ROOT_DIR%\Tools"
 if %errorlevel% equ 0 (goto Execute) else (echo. && echo Throwed error code: %errorlevel% && echo.)
 goto End
 
