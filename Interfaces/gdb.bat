d85e00eae703c2fb2f7ac62a9b6ef13d56b4a24a
rgladkyi <roman.gladkuy@gmail.com>
2017-08-18 16:13:38 +0300

    Added/Updated gdb.bat.

---
+++ Interfaces/gdb.bat
@@ -0,0 +1,20 @@
+@echo off && call cmdex_pre_run.bat
+
+:CheckMinGw
+call check_tool "%CMDEX_ROOT_DIR%\Tools\MinGW\bin\mingw32-gcc-5.3.0.exe" "%CMDEX_ROOT_DIR%\WinOpenInstaller\InstallScripts\mingw_install.bat"  "%CMDEX_ROOT_DIR%\Tools\"
+if %errorlevel% equ 0 (goto ExecuteMinGW) else (echo. && echo Throwed error code: %errorlevel% && echo.)
+goto End
+
+:ExecuteMinGW
+REM Add path
+set PATH=%PATH%;%CMDEX_ROOT_DIR%\Tools\MinGW\bin;
+
+call "gdbserver.exe" %*
+
+REM Undo path
+set PATH=%PATH:;%CMDEX_ROOT_DIR%\Tools\MinGW\bin;=%
+goto End
+
+:End
+
+
